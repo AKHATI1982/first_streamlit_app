1e6493e62a1be3a8bd7a86a2adaa1c22e5b795a6
AKHATI1982 <133806112+AKHATI1982@users.noreply.github.com>
2023-05-17 08:28:39 -0500

    Update streamlit_app.py
    Added Fruit Advice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,6 @@ streamlit.dataframe(fruits_to_show)
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
